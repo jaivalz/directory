e7a1f846ddb4192b575e9e70e4435cae390ef628
Jaime <jaivalz@yahoo.com>
2015-07-14 22:47:17 +0000

    Added reviews

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,11 +1,13 @@
 class Place < ActiveRecord::Base
-  validates_presence_of :name
-  validates_presence_of :phone
-  validates_presence_of :address
-  validates_presence_of :website
-  validates_presence_of :user_id
-  belongs_to :user
-  
-  geocoded_by :address
-  after_validation :geocode
+	validates_presence_of :name
+	validates_presence_of :phone
+	validates_presence_of :address
+	validates_presence_of :website
+	validates_presence_of :user_id
+	belongs_to :user
+
+	geocoded_by :address
+	after_validation :geocode
+
+	has_many :reviews, dependent: :destroy
 end
