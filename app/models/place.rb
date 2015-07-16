c7ed4e072838d3d6147eadf6b662210de9dd8cf5
Jaime <jaivalz@yahoo.com>
2015-07-16 22:18:54 +0000

    Added search funtion to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -10,10 +10,18 @@ class Place < ActiveRecord::Base
 	after_validation :geocode
 
 	has_many :reviews, dependent: :destroy
-  
-  def average_rating
-    self.reviews.sum(:score) / reviews.size
-  rescue ZeroDivisionError
-    0
-  end
+
+	def average_rating
+		self.reviews.sum(:score) / reviews.size
+	rescue ZeroDivisionError
+		0
+	end
+
+	def self.search(search)
+		if search
+			where(['name LIKE ? OR address LIKE ?', "#{search}", "#{search}"])
+		else
+			all
+		end
+	end
 end
